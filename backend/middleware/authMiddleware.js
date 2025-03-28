6a2b958132a9618c79a280caa7dcdf0002e9f6aa
johnrayrafaela <johnrayrafaela13@gmail.com>
2025-03-28 14:16:59 +0800

    modified Admin dashboard feature and add services

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,16 +1,18 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-// const authMiddleware = (req, res, next) => {
-//   const token = req.header("Authorization");
-//   if (!token) return res.status(401).json({ message: "Access Denied" });
+exports.verifyAdmin = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1]; 
 
-//   try {
-//     const verified = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = verified;
-//     next();
-//   } catch (err) {
-//     res.status(400).json({ message: "Invalid Token" });
-//   }
-// };
+  if (!token) return res.status(401).json({ message: "Access denied" });
 
-// module.exports = authMiddleware;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ message: "Forbidden: Admin access only" });
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
